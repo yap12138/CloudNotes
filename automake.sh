0f05e3dd62ca95a379345db8a5ed02762225ec8c
yap12138 <438773897@qq.com>
2019-01-16 14:53:40 +0800

    add auto make shell script

---
+++ automake.sh
@@ -0,0 +1,38 @@
+do_make() {
+	src=$1
+	dst=${src%.cpp*}
+	
+	valid=true
+	if [ ! -f "${src}" ]; then
+		valid=false
+		echo "error: no a source file."
+	fi
+	
+	if [ ! "${valid}" = true ]; then
+		echo "usage: $0 <srcfile>"
+		exit 1
+	fi
+	
+	echo "g++ -g -Wall -std=c++14 -o ${dst} ${src}"
+	g++ -g -Wall -std=c++14 -o ${dst} ${src}
+}
+
+do_clean() {
+	for var in $(ls)
+	do
+		if [ $1 != $var -a -x $var ]; then
+			echo "rm $var"
+			rm $var
+		fi
+	done
+}
+
+if [ $# -lt 1 ]; then
+	echo "error: wrong param."
+	echo "usage: $0 <srcfile>"
+	exit 1
+elif [ $1 != "clean" ]; then
+	do_make $*
+else
+	do_clean ${0#*/}
+fi
